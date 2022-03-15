d042c64bbdaba35e1dd8e6f44eb184364857b74f
soyOswado <oswaldosanchezmelos@gmail.com>
2022-03-15 03:14:00 +0000

    Agregando el router correcto

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,7 +10,7 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard', icon: 'ni-tv-2 text-primary', class: '' },
   { path: '/user-profile', title: 'Perfil', icon: 'ni-single-02 text-yellow', class: '' },
-  { path: '/dish', title: 'Tables', icon: 'ni-bullet-list-67 text-red', class: '' },
+  { path: '/dish', title: 'Dish', icon: 'ni-bullet-list-67 text-red', class: '' },
   { path: '/login', title: 'Login', icon: 'ni-key-25 text-info', class: '' },
   //{ path: '/register', title: 'Register', icon: 'ni-circle-08 text-pink', class: '' }
 ];
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -4,12 +4,12 @@ import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { IconsComponent } from '../../pages/icons/icons.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
+import { DishComponent } from '../../pages/dish/dish.component';
 
 export const AdminLayoutRoutes: Routes = [
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'tables',         component: TablesComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent }
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'user-profile', component: UserProfileComponent },
+    { path: 'dish', component: DishComponent },
+    { path: 'icons', component: IconsComponent },
+    { path: 'maps', component: MapsComponent }
 ];
